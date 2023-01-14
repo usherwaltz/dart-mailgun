077299095df1b6c3e4b9f0b0f4cd51e01786830b
beccauwu <93547901+beccauwu@users.noreply.github.com>
2023-01-14 01:06:00 +0100

    create class for options and params

--- lib/src/mailgun_types.dart
+++ lib/src/mailgun_types.dart
@@ -1,10 +1,245 @@
 import 'dart:convert';
 
-class MailgunOptions {
-  Map<String, dynamic>? templateVariables;
-  @override
-  String toString() {
-    return jsonEncode(templateVariables);
+import 'package:http/http.dart';
+import 'package:intl/intl.dart';
+
+class DeliveryTimeOptimizePeriod {
+  int min;
+  int max;
+  DeliveryTimeOptimizePeriod(this.min, this.max);
+  get value => '$min-$max';
+}
+
+enum MGPlanType { scale, other }
+
+enum TrackingClicks { yes, no, htmlonly }
+
+class MGCustomHeader {
+  final String _value;
+  MGCustomHeader._(this._value);
+  factory MGCustomHeader.fromString(String value) {
+    final headerRegex = RegExp(r'^h:X-[a-zA-Z-]+$', caseSensitive: false);
+    if (!headerRegex.hasMatch(value))
+      throw FormatException(
+          "String must be in format 'h:X-my-header' where my-header is the custom header name");
+    return MGCustomHeader._(value);
+  }
+  String get value => _value;
+}
+
+class MGOptions {
+  MGPlanType planType;
+  bool? testMode;
+  DateTime? deliveryTime;
+  List<String>? tags;
+  bool? dkim;
+  String? _deliveryTimeOptimizePeriod;
+  set deliveryTimeOptimizePeriod(int? value) {
+    if (planType != MGPlanType.scale) {
+      throw Exception(
+          'o:deliverytime-optimize-period is only available for scale plans');
+    }
+    if (value == null) {
+      _deliveryTimeOptimizePeriod = null;
+      return;
+    }
+    if (value < 24 || value > 72) {
+      throw Exception('deliveryTimeOptimizePeriod must be between 24 and 72');
+    }
+    _deliveryTimeOptimizePeriod = '${value}H';
+  }
+
+  int? get deliveryTimeOptimizePeriod => _deliveryTimeOptimizePeriod == null
+      ? null
+      : int.parse(_deliveryTimeOptimizePeriod!
+          .substring(0, _deliveryTimeOptimizePeriod!.length - 1));
+  DateTime? _timeZoneLocalize;
+  set timeZoneLocalize(String? value) {
+    if (value == null) {
+      _timeZoneLocalize = null;
+      return;
+    }
+    if (planType != MGPlanType.scale) {
+      throw Exception('o:time-zone-localize is only available for scale plans');
+    }
+    _timeZoneLocalize = DateFormat('j:m').parse(value);
+  }
+
+  String? get timeZoneLocalize => _timeZoneLocalize?.toString();
+  bool? tracking;
+  String? _trackingClicks;
+  set trackingClicks(TrackingClicks? value) {
+    if (value == null) {
+      _trackingClicks = null;
+      return;
+    }
+    _trackingClicks = value.toString().split('.').last;
+  }
+
+  TrackingClicks? get trackingClicks => _trackingClicks == null
+      ? null
+      : TrackingClicks.values.firstWhere(
+          (element) => element.toString().split('.').last == _trackingClicks);
+  bool? trackingOpens;
+  bool? requireTLS;
+  bool? skipVerification;
+  Map<String, String>? _customHeaders;
+  set customHeaders(Map<String, String>? value) {
+    if (value == null) {
+      _customHeaders = null;
+      return;
+    }
+    _customHeaders = value.map((key, value) {
+      key = key.toLowerCase().startsWith('h:x-') ? key : 'h:X-$key';
+      return MapEntry(key, value);
+    });
+  }
+
+  Map<String, String>? get customHeaders =>
+      _customHeaders == null ? null : _customHeaders;
+  Map<String, String>? _customVars;
+  set customVars(Map<String, String>? value) {
+    if (value == null) {
+      _customVars = null;
+      return;
+    }
+    _customVars = value.map((key, value) {
+      key = key.toLowerCase().startsWith('v:') ? key : 'v:$key';
+      return MapEntry(key, value);
+    });
+  }
+
+  Map<String, String>? get customVars => _customVars;
+  Map<String, String>? recipientVars;
+  MGOptions(
+      int? deliveryTimeOptimizePeriod,
+      String? timeZoneLocalize,
+      TrackingClicks? trackingClicks,
+      Map<String, String>? customHeaders,
+      Map<String, String>? customVars,
+      {this.planType = MGPlanType.other,
+      this.testMode,
+      this.deliveryTime,
+      this.tags,
+      this.dkim,
+      this.tracking,
+      this.trackingOpens,
+      this.requireTLS,
+      this.skipVerification,
+      this.recipientVars}) {
+    this.deliveryTimeOptimizePeriod = deliveryTimeOptimizePeriod;
+    this.timeZoneLocalize = timeZoneLocalize;
+    this.trackingClicks = trackingClicks;
+    this.customHeaders = customHeaders;
+    this.customVars = customVars;
+  }
+  MultipartRequest configureRequest(MultipartRequest request) {
+    if (planType == MGPlanType.scale) {
+      request.fields['o:deliverytime-optimize-period'] =
+          _deliveryTimeOptimizePeriod ?? '';
+      request.fields['o:time-zone-localize'] =
+          _timeZoneLocalize?.toIso8601String() ?? '';
+    }
+    request.fields['o:testmode'] = testMode?.toString() ?? '';
+    request.fields['o:deliverytime'] = deliveryTime?.toIso8601String() ?? '';
+    request.fields['o:tag'] = tags?.join(',') ?? '';
+    request.fields['o:dkim'] = dkim?.toString() ?? '';
+    request.fields['o:tracking'] = tracking?.toString() ?? '';
+    request.fields['o:tracking-clicks'] = _trackingClicks ?? '';
+    request.fields['o:tracking-opens'] = trackingOpens?.toString() ?? '';
+    request.fields['o:require-tls'] = requireTLS?.toString() ?? '';
+    request.fields['o:skip-verification'] = skipVerification?.toString() ?? '';
+    _customHeaders?.forEach((key, value) {
+      request.fields[key] = value;
+    });
+    _customVars?.forEach((key, value) {
+      request.fields[key] = value;
+    });
+    request.fields['recipient-variables'] = json.encode(recipientVars ?? {});
+    return request;
+  }
+
+  Map<String, String> toMap() {
+    return {
+      'o:testmode': testMode?.toString() ?? '',
+      'o:deliverytime': deliveryTime?.toIso8601String() ?? '',
+      'o:tag': tags?.join(',') ?? '',
+      'o:dkim': dkim?.toString() ?? '',
+      'o:tracking': tracking?.toString() ?? '',
+      'o:tracking-clicks': _trackingClicks ?? '',
+      'o:tracking-opens': trackingOpens?.toString() ?? '',
+      'o:require-tls': requireTLS?.toString() ?? '',
+      'o:skip-verification': skipVerification?.toString() ?? '',
+      if (planType == MGPlanType.scale)
+        'o:deliverytime-optimize-period':
+            _deliveryTimeOptimizePeriod?.toString() ?? '',
+      if (planType == MGPlanType.scale)
+        'o:time-zone-localize': _timeZoneLocalize?.toIso8601String() ?? '',
+      'recipient-variables': json.encode(recipientVars ?? {}),
+      ...?_customHeaders,
+      ...?_customVars
+    };
+  }
+}
+
+class IMGSenderParams {
+  List<String> from;
+  List<String> to;
+  String subject;
+  Content content;
+  List<String>? cc;
+  List<String>? bcc;
+  List<String>? tags;
+  List<String>? attachments;
+  List<String>? inline;
+  MGOptions? options;
+  IMGSenderParams(this.from, this.to, this.subject, this.content,
+      {this.cc,
+      this.bcc,
+      this.tags,
+      this.attachments,
+      this.inline,
+      this.options});
+  MultipartRequest configureRequest(MultipartRequest request) {
+    request.fields['from'] = from.join(',');
+    request.fields['to'] = to.join(',');
+    request.fields['subject'] = subject;
+    switch (content.type) {
+      case ContentType.html:
+        request.fields['html'] = content.value;
+        break;
+      case ContentType.text:
+        request.fields['text'] = content.value;
+        break;
+      case ContentType.template:
+        request.fields['template'] = content.value;
+        request.fields['h:X-Mailgun-Variables'] = content.templateVariables;
+        break;
+      default:
+        throw FormatException('Invalid content type given', content.type);
+    }
+    request.fields['cc'] = cc?.join(',') ?? '';
+    request.fields['bcc'] = bcc?.join(',') ?? '';
+    request.fields['o:tag'] = tags?.join(',') ?? '';
+    request.fields['attachment'] = attachments?.join(',') ?? '';
+    request.fields['inline'] = inline?.join(',') ?? '';
+    options?.configureRequest(request);
+    return request;
+  }
+
+  Map<String, String> toMap() {
+    return {
+      'from': from.join(','),
+      'to': to.join(','),
+      'subject': subject,
+      'cc': cc?.join(',') ?? '',
+      'bcc': bcc?.join(',') ?? '',
+      'o:tag': tags?.join(',') ?? '',
+      'attachment': attachments?.join(',') ?? '',
+      'inline': inline?.join(',') ?? '',
+      ...content.toMap(),
+      ...?options?.toMap()
+    };
   }
 }
 
@@ -18,7 +253,6 @@ class MGResponse {
 
   MGResponse(this.status, this.message);
 }
-
 
 /// The `Content` class represents the content of an email.
 ///
@@ -45,15 +279,16 @@ class MGResponse {
 /// var templateContent = Content.template("test", {"test": "test"});
 /// ```
 class Content {
-
   /// The `value` field holds the actual content of the email.
   String value;
+
   /// The `_templateVariables` field is a map that holds the variables that will be replaced in the template.
   ///
   /// Note:
   /// -------
   /// This field is only set when creating an instance of the class using the `Content.template()` constructor.
   late Map<String, dynamic> _templateVariables;
+
   /// The private `_type` field holds the type of content.
   ///
   /// The possible values for this field are `ContentType.html`, `ContentType.text`, and `ContentType.template`.
@@ -62,21 +297,23 @@ class Content {
   /// The `Content.html(value)` constructor
   /// * initialises the class with `value` as html content
   /// * sets the `_type` field to `ContentType.html`.
-  Content.html(this.value){
+  Content.html(this.value) {
     this.value = value;
     this._type = ContentType.html;
   }
+
   /// The `Content.text(value)` constructor
   /// * initialises the class with `value` as text content
   /// * sets the `_type` field to `ContentType.text`.
-  Content.text(this.value){
+  Content.text(this.value) {
     this.value = value;
     this._type = ContentType.text;
   }
+
   /// The `Content.template(value, templateVariables)` constructor
   /// * initialises the class with `value` as the template name and `_templateVariables` as a map of variables.
   /// * sets the `_type` field to `ContentType.template`.
-  Content.template(this.value, this._templateVariables){
+  Content.template(this.value, this._templateVariables) {
     this._type = ContentType.template;
   }
 
@@ -92,6 +329,18 @@ class Content {
 
   /// The `type` getter returns the type of content.
   ContentType get type => _type;
+  Map<String, dynamic> toMap() {
+    switch (_type) {
+      case ContentType.html:
+        return {'html': value};
+      case ContentType.text:
+        return {'text': value};
+      case ContentType.template:
+        return {'template': value, 'h:X-Mailgun-Variables': templateVariables};
+      default:
+        throw FormatException('Invalid content type given', _type);
+    }
+  }
 }
 
 /// The `IMailgunSender` interface is a base class for sending emails with the mailgun API.
@@ -113,7 +362,6 @@ class Content {
 /// }
 /// ```
 abstract class IMailgunSender {
-
   /// The `send()` method sends an email using the mailgun API.
   ///
   /// Parameters:
@@ -125,7 +373,7 @@ abstract class IMailgunSender {
   /// * `cc` - a list of email addresses of the recipients to be copied.
   /// * `bcc` - a list of email addresses of the recipients to be blind copied.
   /// * `attachments` - a list of attachments to be sent with the email.
-  /// * `options` - an instance of the `MailgunOptions` class.
+  /// * `options` - an instance of the `MGOptions` class.
   /// * `useDifferentFromDomain` - a boolean value indicating whether to use a different domain for the sender.
   ///
   /// Returns:
@@ -133,14 +381,5 @@ abstract class IMailgunSender {
   /// * `Future<MGResponse>` - an instance of the `MGResponse` class.
   ///   * `MGResponse.status` - the status of the request.
   ///   * `MGResponse.message` - the message returned by the mailgun API.
-  Future<MGResponse> send(
-      {String from = 'mailgun',
-      required List<String> to,
-      required String subject,
-      required Content content,
-      List<String>? cc,
-      List<String>? bcc,
-      List<dynamic> attachments = const [],
-      MailgunOptions? options,
-      bool? useDifferentFromDomain});
+  Future<MGResponse> send(IMGSenderParams params);
 }
