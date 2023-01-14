be5d49349fbbe98f5dc522c3a65065fff3519785
beccauwu <93547901+beccauwu@users.noreply.github.com>
2023-01-14 01:06:22 +0100

    refactor for params class

--- lib/src/mailgun_mailer.dart
+++ lib/src/mailgun_mailer.dart
@@ -12,16 +12,7 @@ class MailgunSender implements IMailgunSender {
   MailgunSender(
       {required this.domain, required this.apiKey, this.regionIsEU = false});
 
-  Future<MGResponse> send(
-      {from = 'mailgun',
-      required to,
-      required subject,
-      required content,
-      cc,
-      bcc,
-      attachments = const [],
-      options,
-      useDifferentFromDomain}) async {
+  Future<MGResponse> send(params) async {
     var client = http.Client();
     var host = regionIsEU ? 'api.eu.mailgun.net' : 'api.mailgun.net';
     try {
@@ -32,44 +23,7 @@ class MailgunSender implements IMailgunSender {
               scheme: 'https',
               host: host,
               path: '/v3/$domain/messages'));
-      request.fields['subject'] = subject;
-      request.fields['from'] = from;
-      request.fields['cc'] = cc?.join(", ") ?? '';
-      request.fields['bcc'] = bcc?.join(", ") ?? '';
-      switch (content.type) {
-        case ContentType.html:
-          request.fields['html'] = content.value;
-          break;
-        case ContentType.text:
-          request.fields['text'] = content.value;
-          break;
-        case ContentType.template:
-          request.fields['template'] = content.value;
-          request.fields['h:X-Mailgun-Variables'] = content.templateVariables;
-          break;
-        default:
-          throw Exception('Invalid content type');
-      }
-
-      if (to.length > 0) {
-        request.fields['to'] = to.join(", ");
-      }
-      if (options != null) {
-        if (options.templateVariables != null) {
-          request.fields['h:X-Mailgun-Variables'] =
-              jsonEncode(options.templateVariables);
-        }
-      }
-      if (attachments.length > 0) {
-        request.headers["Content-Type"] = "multipart/form-data";
-        for (var i = 0; i < attachments.length; i++) {
-          var attachment = attachments[i];
-          if (attachment is File) {
-            request.files.add(await http.MultipartFile.fromPath(
-                'attachment', attachment.path));
-          }
-        }
-      }
+      request = params.configureRequest(request);
       var response = await client.send(request);
       var responseBody = await response.stream.bytesToString();
       var jsonBody = jsonDecode(responseBody);
